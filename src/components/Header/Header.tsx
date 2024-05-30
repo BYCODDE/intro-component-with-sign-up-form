7a252b8692adc2fd3df5b075b49b4c4ecb026cb4
BYCODDE <anaraliev217@gmail.com>
2024-05-30 16:05:14 +0400

    mobile markup done!

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
         scripted tutorials is great, but understanding how developers think is
         invaluable.
       </p>
-      <div className="rounded-[10px] bg-[#5E54A4]  max-h-[88px] font-[400] text-[15px] leading-[26px] p-[18px] shadow-custom  mb-[24px]">
+      <div className="rounded-[10px] bg-[#5E54A4]  max-h-[88px] font-[400] text-[15px] leading-[26px] p-[18px] shadow-custom  mb-[24px] cursor-pointer">
         <span className="font-bold">Try it free 7 days</span> then <br />
         $20/mo. thereafter
       </div>
